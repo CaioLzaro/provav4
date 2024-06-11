dc622bbd174b43ccff2caab22bb4afc6beee6519
CaioLzaro <169152145+CaioLzaro@users.noreply.github.com>
2024-06-11 09:10:30 -0300

    Update 1q.py

--- 1q.py
+++ 1q.py
@@ -1,12 +1,3 @@
-#Faça uma função que retorne o reverso de um número inteiro informado
-# n = str(input("Digite um número inteiro:"))
-# def reverso(n):
-#       nstr = len(n)
-#       nstr(reversed)
-#       nint = int(nstr)
-#       return nint
-      
-
 input = str(input("Digite o número:"))
 input_invertido = input[::-1]
 print(input_invertido)
